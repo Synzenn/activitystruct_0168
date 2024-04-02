88cf5072e8e1bfaa1a8dc5a98ca0e183a5aa08a4
Synzenn <velriel42@gmail.com>
2024-04-02 13:03:43 +0700

    membuat pemanggilan ddidalam for

--- Part4/Part4.cpp
+++ Part4/Part4.cpp
@@ -34,5 +34,15 @@ int main() {
 		cout << endl;
 	}
 
-	
+	for (int i = 0; i < 3; i++) {
+
+		cout << endl;
+		cout << "Data Mahasiwa Ke-" << (i + 1) << ":" << endl;
+		cout << "\n NIM :" << mhs[i].nim;
+		cout << "\n Nama :" << mhs[i].nama;
+		cout << "\n Alamat :";
+		cout << "\n \t Desanya :" << mhs[i].alamat.desa;
+		cout << "\n \t Kotanya :" << mhs[i].alamat.kota;
+		cout << "\n Umur :" << mhs[i].umur;
+	}
 }
