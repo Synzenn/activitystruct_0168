8487ae98a621d0e9fe69ee5a28d420b19bdc9fe0
Synzenn <velriel42@gmail.com>
2024-04-02 13:03:15 +0700

    menyelesaikan pembuatan looping for 1

--- Part4/Part4.cpp
+++ Part4/Part4.cpp
@@ -17,6 +17,22 @@ struct Mahasiswa {
 int main() {
 	Mahasiswa mhs[3];
 	for (int i = 0; i < 3; i++){
-		
+		cout << "Data ke-" << (i + 1) << ":" << endl;
+		cout << "Nomor Mahasiswa :";
+		getline(cin, mhs[i].nim);
+		cout << "Nama Mahasiswa :";
+		getline(cin, mhs[i].nama);
+
+		cout << "Alamat Mahasiswa : " << endl;
+		cout << "\t Desa :";
+		cin >> mhs[i].alamat.desa;
+		cout << "\t Kota :";
+		cin >> mhs[i].alamat.kota;
+		cout << "Umur Mahasiswa : ";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+		cout << endl;
 	}
+
+	
 }
