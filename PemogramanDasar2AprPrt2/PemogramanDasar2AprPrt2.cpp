6f45ac138cb542d4d4c1954082789768adb27587
Synzenn <velriel42@gmail.com>
2024-04-02 11:58:43 +0700

    membuat pemanggilanl dan menampilkannya

--- PemogramanDasar2AprPrt2/PemogramanDasar2AprPrt2.cpp
+++ PemogramanDasar2AprPrt2/PemogramanDasar2AprPrt2.cpp
@@ -27,4 +27,11 @@ int main() {
 	cout << "Umur Mahasiswa : ";
 	cin >> mhs.umur;
 
+	cout << endl;
+	cout << "\n NIM :" << mhs.nim;
+	cout << "\n Nama :" << mhs.nama;
+	cout << "\n Alamat :";
+	cout << "\n \t Desanya :" << mhs.alamat.desa;
+	cout << "\n \t Kotanya :" << mhs.alamat.kota;
+	cout << "\n Umur :" << mhs.umur;
 };
